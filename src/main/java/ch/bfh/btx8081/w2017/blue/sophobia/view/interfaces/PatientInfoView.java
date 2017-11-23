9620d6e1206a4b51f82f170864a8c4f92156e0d5
kybup1 <philip.kyburz@students.bfh.ch>
2017-11-23 17:48:36 +0100

    PatientInfoView (Interface) and PatientInfoClickListener: Done

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/interfaces/PatientInfoView.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/interfaces/PatientInfoView.java
@@ -1,5 +1,16 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces;
 
 public interface PatientInfoView {
-
+	
+	public void setBirthdate(date);
+	public void setDiagnosis(String);
+	public void setDrugs(String);
+	public void setAddress(String);
+	public void fillNoteList(NoteList);
+	public Note getSelectedNote();
+	public void addListener();
+	
+	public Interface PatientInfoClickListener{
+		public buttonClick();
+	}
 }
