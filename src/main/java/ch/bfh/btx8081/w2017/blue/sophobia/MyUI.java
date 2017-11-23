8aac93e2a9f1a79d42e1c7dc36079a332693cd26
Jonathan Meier <jnt.meier@gmail.com>
2017-11-23 11:17:06 +0100

    change svnignore

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/MyUI.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/MyUI.java
@@ -31,7 +31,7 @@ public class MyUI extends UI {
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
-        Button button = new Button("Klicken sie mich hart");
+        Button button = new Button("Klicken sie mich");
         button.addClickListener(e -> {
             layout.addComponent(new Label("Thanks " + name.getValue() 
                     + ", it works my master!"));
