9da8cdbc42cae442f058be131c021b1fc90b43ef
petim1 <32766548+petim1@users.noreply.github.com>
2017-11-24 11:13:08 +0100

    DrugList V1

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DrugList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DrugList.java
@@ -1,5 +1,30 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
-public class DrugList {
+import java.util.ArrayList;
 
+public class DrugList {
+	ArrayList<String> drugs = new ArrayList<String>();
+	int randNum, minimum = 0, maximum = 7;
+
+	public void initdrugs() {
+
+		drugs.add("Medikament: Citalopram / Wirkstoff: Citalopram  / Dosis: 40mg");
+		drugs.add("Medikament: Cipralex / Wirkstoff: Escitalopram / Dosis: 10mg");
+		drugs.add("Medikament: Cipralex / Wirkstoff: Escitalopram / Dosis: 20mg");
+		drugs.add("Medikament: Lyrica / Wirkstoff: Pregabalin / Dosis: 150mg");
+		drugs.add("Medikament: Lyrica / Wirkstoff: Pregabalin / Dosis: 300mg");
+		drugs.add("Medikament: Cymbalta / Wirkstoff: Duloxetin / Dosis: 30mg");
+		drugs.add("Medikament: Cymbalta / Wirkstoff: Duloxetin / Dosis: 60mg");
+		drugs.add("Medikament: Sertralin / Wirkstoff: Sertralin / Dosis: 50mg");
+
+		randNum = minimum + (int) (Math.random() * maximum);
+
+	}
+
+	@Override
+	public String toString() {
+		initdrugs();
+		return drugs.get(randNum);
+
+	}
 }
