efbf3a0be992c11941b98a2a348881610c95dbd9
kybup1 <philip.kyburz@students.bfh.ch>
2017-11-24 11:43:36 +0100

    modified ObjectiveList class: Added attributes added createObjective method

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/ObjectiveList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/ObjectiveList.java
@@ -1,5 +1,42 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
-public class ObjectiveList {
+import java.util.List;
 
+import javax.persistence.OneToMany;
+
+/**
+ * Manages all objectives for a Patient
+ * @author kybup1
+ *
+ */
+public class ObjectiveList {
+	
+	private int olid;
+	
+	@OneToMany(mappedBy= "bojList")
+	private List<Objective> objectives;
+	
+	/**
+	 * Creates a new Objective in this List
+	 * @param name of the new objective
+	 * @param description of the new objective
+	 * @param  difficulty of the objective as Integer from 1 to 10
+	 */
+	public void createObj(String name, String description, int difficulty){
+		Objective obj = new Objective();
+		obj.setName(name);
+		obj.setDescription(description);
+		obj.setDifficulty(difficulty);
+		obj.setComplete(false);
+		
+		objectives.add(obj);
+	}
+
+	public int getOlid() {
+		return olid;
+	}
+
+	public void setOlid(int olid) {
+		this.olid = olid;
+	}
 }
