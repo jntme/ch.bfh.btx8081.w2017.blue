12ba97189424192d322c9b5c89a98b2748a6b2cf
kybup1 <philip.kyburz@students.bfh.ch>
2017-11-24 10:55:17 +0100

    Created Objective class (model)

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/Objective.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/Objective.java
@@ -1,5 +1,51 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
-public class Objective {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Objective {
+	
+	@Id
+	@GeneratedValue
+	private int oid;
+	private String name;
+	private String description;
+	private int difficulty;
+	private boolean isComplete;
+	
+	public int getOid() {
+		return oid;
+	}
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+	public boolean isComplete() {
+		return isComplete;
+	}
+	public void setComplete(boolean isComplete) {
+		this.isComplete = isComplete;
+	}
+	
+	
+	
 }
