a00841d1799234e239ffb8f2223062462415704a
petim1 <32766548+petim1@users.noreply.github.com>
2017-11-24 11:34:09 +0100

    DrugList with comments

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DrugList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DrugList.java
@@ -2,12 +2,26 @@ package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
 import java.util.ArrayList;
 
+
+/**
+* The class DrugList provides dummy-data for drugs
+* to display in patient information.
+*
+* @author  petim1
+* @version 1.0
+* @since   24.11.2017 
+*/
 public class DrugList {
+	//Variables for class - use
 	ArrayList<String> drugs = new ArrayList<String>();
 	int randNum, minimum = 0, maximum = 7;
 
+	/**
+	 * The method {@code initdrugs()} fills the arraylist with dummy-data
+	 * and the random generator generates a number
+	 * 
+	 */
 	public void initdrugs() {
-
 		drugs.add("Medikament: Citalopram / Wirkstoff: Citalopram  / Dosis: 40mg");
 		drugs.add("Medikament: Cipralex / Wirkstoff: Escitalopram / Dosis: 10mg");
 		drugs.add("Medikament: Cipralex / Wirkstoff: Escitalopram / Dosis: 20mg");
@@ -17,14 +31,20 @@ public class DrugList {
 		drugs.add("Medikament: Cymbalta / Wirkstoff: Duloxetin / Dosis: 60mg");
 		drugs.add("Medikament: Sertralin / Wirkstoff: Sertralin / Dosis: 50mg");
 
+		//Generate random number
 		randNum = minimum + (int) (Math.random() * maximum);
 
 	}
 
+	/**
+	 * Overrides the {@code toString()} Method for our specific use
+	 */
 	@Override
 	public String toString() {
 		initdrugs();
 		return drugs.get(randNum);
+		//System.out.print(drugs.get(randNum));
+		//return null;
 
 	}
 }
