21f415dc934d244b3d0b3af051928dfbf6958649
Odaoj1 <hisllaan@yahoo.com>
2017-11-24 10:43:06 +0100

    DiagnosisList

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DiagnosisList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DiagnosisList.java
@@ -1,5 +1,24 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DiagnosisList {
+
+	/*
+	 * Contains Diagnosis list
+	 */
+
+	private ArrayList<String> diagnosisList = new ArrayList<>(
+			Arrays.asList("Diagnosis_1", "Diagnosis_2", " Diagnosis_3", " Diagnosis_3", " Diagnosis_4"));
+
+	public DiagnosisList() {
+		this.diagnosisList = diagnosisList;
+	}
+
+	// Displays the list of patient's diagnosis.
+	public String toString() {
+		return this.diagnosisList.toString();
+	}
 
 }
