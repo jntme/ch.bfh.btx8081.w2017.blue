fd162e33ee28425432f6d74f42d34d39866b9230
kybup1 <philip.kyburz@students.bfh.ch>
2017-11-25 19:40:21 +0100

    Modified Note: Added attributes and their getters and setters

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/Note.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/Note.java
@@ -1,5 +1,56 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
-public class Note {
+import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Represents notes with a title, a content and the date when their created/modified.
+ * @author kybup1
+ *
+ */
+
+@Entity
+public class Note {
+	
+	@Id
+	@GeneratedValue
+	private int nid;
+	private String title;
+	private String content;
+	private Date date;
+	private int noteList;
+	
+	public int getNid() {
+		return nid;
+	}
+	public void setNid(int nid) {
+		this.nid = nid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getNoteList() {
+		return noteList;
+	}
+	public void setNoteList(int noteList) {
+		this.noteList = noteList;
+	}
 }
