fe5733832c19053144b29633e15846701cdd814f
kybup1 <philip.kyburz@students.bfh.ch>
2017-11-25 21:52:24 +0100

    Created DangerNote

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DangerNote.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DangerNote.java
@@ -1,5 +1,25 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
+import javax.persistence.Entity;
+
+/**
+ * A special kind of note, which contains important information to sustain the patient safety.
+ * The information has to be checked when the DangerNote is active (boolean)
+ * @author kybup1
+ *
+ */
+@Entity
 public class DangerNote extends Note {
+	
+	boolean active;
+
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
 
 }
