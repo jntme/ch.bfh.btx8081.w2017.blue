5e1d2640c2bcd280aff414879e32707d8ffc4883
kybup1 <philip.kyburz@students.bfh.ch>
2017-11-25 22:16:10 +0100

    updated NoteList: createNote method isDangerNote method

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/NoteList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/NoteList.java
@@ -1,12 +1,20 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
+import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.rmi.CORBA.UtilDelegate;
 
+/**
+ * Manages all notes for a patient
+ * @author kybup1
+ *
+ */
 @Entity
 public class NoteList {
 	
@@ -17,5 +25,45 @@ public class NoteList {
 	@OneToMany(mappedBy= "noteList")
 	private List<Note> notes;
 	
-
+	/**
+	 * Creates a new note.
+	 * Needs the title and the content of the note.
+	 * Depending on the value of the boolean a different kind of note will be created
+	 * true = DangerNote, false=Note
+	 * Furthermore the Systemtime is captured and saved 
+	 * @param title
+	 * @param content
+	 * @param danger
+	 */
+	public void createNote(String title, String content, boolean danger){
+		
+		Date date = new Date(Calendar.getInstance().getTime().getTime());
+		
+		if(danger){
+			DangerNote note = new DangerNote();
+			note.setTitle(title);
+			note.setContent(content);
+			note.setDate(date);
+			note.setActive(true);
+			notes.add(note);
+		} else {
+			Note note = new Note();
+			note.setTitle(title);
+			note.setContent(content);
+			note.setDate(date);
+			notes.add(note);
+		}
+	}
+	
+	/**
+	 * Chechs if a given note is a DangerNote and returns a boolean
+	 * @param note
+	 * @return boolean, true = is a DangerNote
+	 */
+	public boolean isDangerNote(Note note){
+		if (DangerNote.class.isInstance(note))
+			return true;
+		else
+			return false;
+	}
 }
