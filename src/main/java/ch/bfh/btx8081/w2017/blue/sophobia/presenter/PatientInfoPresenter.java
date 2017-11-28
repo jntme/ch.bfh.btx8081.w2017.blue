cb8f59720453ad41ee161600e2ed66c166413447
Odaoj1 <hisllaan@yahoo.com>
2017-11-28 06:58:15 +0100

    patientInfoPresenter

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/presenter/PatientInfoPresenter.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/presenter/PatientInfoPresenter.java
@@ -1,5 +1,33 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.presenter;
 
-public class PatientInfoPresenter {
+import ch.bfh.btx8081.w2017.blue.sophobia.model.ObjectiveList;
+import ch.bfh.btx8081.w2017.blue.sophobia.model.Patient;
+import ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces.PatientInfoView;
+import ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces.PatientInfoView.PatientInfoClickListener;
+import ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces.PatientObjectiveListView;
+/**
+ * @author odaoj1
+ *
+ */
+
+public class PatientInfoPresenter implements PatientInfoClickListener{
+
+private Patient model;
+private PatientInfoView view;
+
+public void initPatientview(){
+}
+public void initObjListPresenter(){
+}
+public void initPatInfoPresenter(){
+}
+private void checkDangerNotes(){
+}
+@Override
+public void buttonClick() {
+	// TODO Auto-generated method stub
+
+}
+
 
 }
