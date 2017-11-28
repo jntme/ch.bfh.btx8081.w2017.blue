52030389faf6c80885e4e6a3154d3aa81cdf7765
petim1 <32766548+petim1@users.noreply.github.com>
2017-11-28 14:39:40 +0100

    change to multiple drugs

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DrugList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DrugList.java
@@ -2,24 +2,24 @@ package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
 import java.util.ArrayList;
 
-
 /**
-* The class DrugList provides dummy-data for drugs
-* to display in patient information.
-*
-* @author  petim1
-* @version 1.0
-* @since   24.11.2017 
-*/
+ * The class DrugList provides dummy-data for drugs to display in patient
+ * information.
+ *
+ * @author petim1
+ * @version 1.0
+ * @since 24.11.2017
+ */
 public class DrugList {
-	//Variables for class - use
+	// Variables for class - use
 	ArrayList<String> drugs = new ArrayList<String>();
-	int randNum, minimum = 0, maximum = 8;
+	ArrayList<String> mainList = new ArrayList<String>();
+	int randNo, minimum = 0, maximum = 8;
 
 	/**
-	 * The method {@code initdrugs()} fills the arraylist with dummy-data
-	 * and the random generator generates a number
-	 * 
+	 * The method {@code initdrugs()} fills the ArrayList with dummy-data and
+	 * the random generator generates a number to add 3 drugs to a list that
+	 * will be given back.
 	 */
 	public void initdrugs() {
 		drugs.add("Medikament: Citalopram / Wirkstoff: Citalopram  / Dosis: 40mg");
@@ -31,8 +31,12 @@ public class DrugList {
 		drugs.add("Medikament: Cymbalta / Wirkstoff: Duloxetin / Dosis: 60mg");
 		drugs.add("Medikament: Sertralin / Wirkstoff: Sertralin / Dosis: 50mg");
 
-		//Generate random number
-		randNum = minimum + (int) (Math.random() * maximum);
+		// Generate random number and add to List
+		for (int i = 0; i < 4; i++) {
+			randNo = minimum + (int) (Math.random() * maximum);
+			mainList.add(drugs.get(randNo));
+
+		}
 
 	}
 
@@ -42,9 +46,7 @@ public class DrugList {
 	@Override
 	public String toString() {
 		initdrugs();
-		return drugs.get(randNum);
-		//System.out.print(drugs.get(randNum));
-		//return null;
+		return mainList.toString();
 
 	}
 }
