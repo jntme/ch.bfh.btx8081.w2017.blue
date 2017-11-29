820dbee617ca3a8406173ccef54f35aec7765a95
Odaoj1 <hisllaan@yahoo.com>
2017-11-29 12:57:14 +0100

    DiagnosisList update

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DiagnosisList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DiagnosisList.java
@@ -2,26 +2,47 @@ package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class DiagnosisList {
-	
+
 	@Id
 	private int diagID;
-	
 
 	/*
 	 * Contains Diagnosis list
 	 */
 
 	private ArrayList<String> diagnosisList = new ArrayList<>(
-			Arrays.asList("Diagnosis_1", "Diagnosis_2", " Diagnosis_3", " Diagnosis_3", " Diagnosis_4"));
+			Arrays.asList("Social Anxiety", "Fear public performan",
+					"Diminished interest or loss of pleasure in almost all activities",
+					"Psychomotor agitation or retardation ", " Feelings of worthlessness"));
 
-	public DiagnosisList() {
-		this.diagnosisList = diagnosisList;
+	//Can we add a method? is it toString method?
+//	public String getDiagnosis(){
+//		Random random = new Random();
+//		int sizeArray = diagnosisList.size();
+//		int randNum = random.nextInt(sizeArray);
+//		String element = diagnosisList.get(randNum);
+//		return element;
+//	}
+
+	//Example
+	public ArrayList<String> getDiagnosisList(){
+		Random random = new Random();
+		int sizeArray = diagnosisList.size();
+
+		ArrayList<String> arl = new ArrayList();
+		for(int i = 0; i<=4;i++){
+			int randNum = random.nextInt(sizeArray);
+			String element = diagnosisList.get(randNum);
+			arl.add(element);
+		}
+		return arl;
 	}
 
 	// Displays the list of patient's diagnosis.
