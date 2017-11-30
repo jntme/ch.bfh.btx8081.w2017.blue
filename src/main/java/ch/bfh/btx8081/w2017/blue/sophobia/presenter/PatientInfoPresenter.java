5991a820a87a0618b28ee21cb05f2fb488377e3f
Odaoj1 <hisllaan@yahoo.com>
2017-11-30 21:56:55 +0100

    PatientInfoPresenter commented

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/presenter/PatientInfoPresenter.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/presenter/PatientInfoPresenter.java
@@ -10,37 +10,46 @@ import java.util.ArrayList;
 
 import ch.bfh.btx8081.w2017.blue.sophobia.model.NoteList;
 import ch.bfh.btx8081.w2017.blue.sophobia.model.ObjectiveList;
+
 /**
  * @author odaoj1
  *
  */
 
-public class PatientInfoPresenter implements PatientInfoClickListener{
+	public class PatientInfoPresenter implements PatientInfoClickListener {
 
-private Patient pat;
-private PatientInfoView patView;
+	private Patient pat;
+	private PatientInfoView patView;
 
-public PatientInfoPresenter(Patient pat, PatientInfoView patview){
-	this.pat = pat;
-	this.patView = patview;
-	initPatInfoView();
-}
+	public PatientInfoPresenter(Patient pat, PatientInfoView patview) {
+		this.pat = pat;
+		this.patView = patview;
+		initPatInfoView();
+	}
+	/**
+	 * initializes Patient view
+	 *
+	 * @author odaoj1
+	 *
+	 */
+	private void initPatInfoView() {
 
-private void initPatInfoView(){
+	}
+	/**
+	 * opens the List of Patient Notes
+	 *
+	 * @author odaoj1
+	 *
+	 */
+	public void openNote() {
+		NoteList noteList = pat.getNoteList();
+		System.out.println(noteList.toString());
+	}
 
-}
+	@Override
+	public void buttonClick() {
+		// TODO Auto-generated method stub
 
-public void openNote(){
-	//ArrayList<NoteList> notesList = pat.getNoteList();
-}
-
-@Override
-public void buttonClick() {
-	// TODO Auto-generated method stub
-
-}
-
-
-
+	}
 
 }
