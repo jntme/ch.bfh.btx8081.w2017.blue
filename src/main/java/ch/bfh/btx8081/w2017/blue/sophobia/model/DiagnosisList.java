88a4390afec5d57605d0faf98973a1b37be90237
Odaoj1 <hisllaan@yahoo.com>
2017-11-30 22:15:46 +0100

    DiagnosisList modified

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DiagnosisList.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/DiagnosisList.java
@@ -8,7 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+/**
+ * Manages list of patient Diagnosis
+ * @author Odaoj1
+ *
+ */
 @Entity
 public class DiagnosisList {
 
@@ -16,37 +20,33 @@ public class DiagnosisList {
 	@GeneratedValue(strategy = GenerationType.TABLE)
 	private int diagID;
 
-	/*
+	/**
 	 * Contains Diagnosis list
 	 */
 
 	private ArrayList<String> diagnosisList = new ArrayList<>(
 			Arrays.asList("Social Anxiety Disorder A", "Persistent Depressive Disorder,"));
 
-	//Can we add a method? is it toString method?
-//	public String getDiagnosis(){
-//		Random random = new Random();
-//		int sizeArray = diagnosisList.size();
-//		int randNum = random.nextInt(sizeArray);
-//		String element = diagnosisList.get(randNum);
-//		return element;
-//	}
 
-	//Example
-//	public ArrayList<String> getDiagnosisList(){
-//		Random random = new Random();
-//		int sizeArray = diagnosisList.size();
-//
-//		ArrayList<String> arl = new ArrayList();
-//		for(int i = 0; i<=4;i++){
-//			int randNum = random.nextInt(sizeArray);
-//			String element = diagnosisList.get(randNum);
-//			arl.add(element);
-//		}
-//		return arl;
-//	}
 
-	// Displays the list of patient's diagnosis.
+	/**
+	 * generates the random list of patient Diagnosis from the existing Array list of Diagnosis
+	 */
+	public ArrayList<String> getDiagnosisList(){
+		Random random = new Random();
+		int sizeArray = diagnosisList.size();
+
+		ArrayList<String> arl = new ArrayList();
+		for(int i = 0; i<=4;i++){
+			int randNum = random.nextInt(sizeArray);
+			String element = diagnosisList.get(randNum);
+			arl.add(element);
+		}
+		return arl;
+	}
+/**
+ * Displays the list of patient's diagnosis
+ */	
 	public String toString() {
 		return this.diagnosisList.toString();
 	}
