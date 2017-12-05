8b72752e01204e9a97e2670eff5055a1f72792f5
ziegm <32767151+ziegm@users.noreply.github.com>
2017-12-05 13:12:20 +0100

    add ObjectiveNewViewImpl.java and ObjectiveNewView.java

---
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/impl/ObjectiveNewViewImpl.java
@@ -0,0 +1,9 @@
+package ch.bfh.btx8081.w2017.blue.sophobia.view.impl;
+
+/**
+ * @author ziegm1
+ *
+ */
+class ObjectiveNewViewImpl {
+
+}
---
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/interfaces/ObjectiveNewView.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces;
+
+/**
+ * @author ziegm1
+ *
+ */
+public interface ObjectiveNewView {
+
+}
