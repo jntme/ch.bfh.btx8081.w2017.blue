2e3af60ceea9fcd86316332a7b70f995b09cc3c7
petim1 <32766548+petim1@users.noreply.github.com>
2017-12-07 16:09:00 +0100

    add ActivityListView, ActivityListViewImpl and ActivityListPresenter

---
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/impl/ActivityListViewImpl.java
@@ -0,0 +1,31 @@
+package ch.bfh.btx8081.w2017.blue.sophobia.view.impl;
+
+import com.vaadin.ui.Panel;
+
+public class ActivityListViewImpl extends Panel /*implements ActivityListView, ClickListener */ {
+
+//	@Override
+//	public void fillActivityList(ActivityList activityList) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public Activity getSelectedActivity() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public void addListener(ActivityListViewListener listener) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void buttonClick(ClickEvent event) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+
+}
---
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/interfaces/ActivityListView.java
@@ -0,0 +1,17 @@
+package ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces;
+
+import ch.bfh.btx8081.w2017.blue.sophobia.model.Activity;
+import ch.bfh.btx8081.w2017.blue.sophobia.model.ActivityList;
+
+public interface ActivityListView {
+	
+//	public void fillActivityList(ActivityList activityList);
+//	public Activity getSelectedActivity();
+//	
+//	// Listener for the event handling
+//	interface PatientObjectiveListViewListener {
+//		void buttonClick(char operation);
+//	}
+//	
+//	public void addListener(ActivityListViewListener listener);
+}
