23b40c106c40d712213d9c8a8ccac0a5f2db024c
Odaoj1 <hisllaan@yahoo.com>
2017-12-24 13:28:39 +0100

    patHistory

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/impl/PatientHistoryViewImpl.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/view/impl/PatientHistoryViewImpl.java
@@ -1,5 +1,16 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.view.impl;
 
-public class PatientHistoryViewImpl {
+import com.vaadin.ui.Window;
+
+import ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces.PatientContactView;
+import ch.bfh.btx8081.w2017.blue.sophobia.view.interfaces.PatientHistoryView;
+
+public class PatientHistoryViewImpl  extends Window implements PatientHistoryView {
+
+	@Override
+	public void setPatientHistory(String history) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
