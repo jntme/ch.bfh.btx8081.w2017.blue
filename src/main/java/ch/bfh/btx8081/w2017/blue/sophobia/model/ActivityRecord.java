38e6b20c5bd755bfbf532253f8a687175834afd4
ziegm <32767151+ziegm@users.noreply.github.com>
2018-01-02 09:50:37 +0100

    Create new classes: - ActivityRecord - ActivityRecordList

---
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/ActivityRecord.java
@@ -0,0 +1,9 @@
+package ch.bfh.btx8081.w2017.blue.sophobia.model;
+
+/**
+ * @author ziegm
+ *
+ */
+public class ActivityRecord {
+
+}
---
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/model/ActivityRecordList.java
@@ -0,0 +1,10 @@
+package ch.bfh.btx8081.w2017.blue.sophobia.model;
+
+/**
+ * Manages all activity records for the specific activity
+ * @author ziegm
+ *
+ */
+public class ActivityRecordList {
+
+}
