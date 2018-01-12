0935bb6d3e2a0e61ec1e6492e67f6df0207f6a20
petim1 <32766548+petim1@users.noreply.github.com>
2018-01-12 10:47:51 +0100

    change folder structure 2nd time [MINE WORKS]

---
+++ test/ch/bfh/btx8081/w2017/blue/sophobia/model/ActivityTest.java
@@ -0,0 +1,14 @@
+package ch.bfh.btx8081.w2017.blue.sophobia.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ActivityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
