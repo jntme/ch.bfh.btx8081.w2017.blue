bddbfd61666978fbd5d72ceecee801a806ddd9cf
petim1 <32766548+petim1@users.noreply.github.com>
2018-01-12 11:28:37 +0100

    added second patient for demonstration --> DO NOT TOUCH! (pls)

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/HowToUseDB.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/HowToUseDB.java
@@ -33,9 +33,13 @@ import ch.bfh.btx8081.w2017.blue.sophobia.persistence.DB;
  * @author jntme
  */
 public class HowToUseDB {
+	final static DateFormat SDF = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN);
+	final static String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+	final static Path path = Paths.get(basepath + "/WEB-INF/images/dummyUserPic.jpg");
+
 
 	public static void howToUseDb() {
-
+		
 		// first of all, you need to get the EntityManager from the db
 		EntityManager em = DB.getEntityManager();
 
@@ -81,9 +85,9 @@ public class HowToUseDB {
 			babbel.setPrename("Tim");
 			babbel.setGender("m");
 			String dateString = "15.03.1995";
-			DateFormat format = new SimpleDateFormat("dd.mm.yyyy", Locale.GERMAN);
+			
 			try {
-				babbel.setBirthdate(format.parse(dateString));
+				babbel.setBirthdate(SDF.parse(dateString));
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -111,7 +115,7 @@ public class HowToUseDB {
 			NoteList notel = new NoteList();
 			
 			notel.createNote("Aufräumen", "Wenn mal jemand Zeit hat, sollte man bei Tim zuhause mal wieder aufräumen", false);
-			notel.createNote("manchmal Aggro", "Tim kann manchmal sehr aggressiv sein, wenn er mit der Situation überfordert ist.", true);
+			notel.createNote("Aggressivität", "Tim kann manchmal bei Überforderung sehr aggressiv werden.", true);
 			notel.createNote("Note3", "Hier steht ein anderer, nicht allzuwichtiger Text", false);
 			
 			//WORKAROUND!!!!!!
@@ -137,17 +141,13 @@ public class HowToUseDB {
 			babbel.setObjectiveList(objl);
 			
 			ActivityList actl1 = new ActivityList();
-			
 			actl1.createAct("Peinliche Situation", "Wasserglas im Restaurant umschütten");
 			actl1.createAct("Fragen", "Nach einer anderen Schuhgrösse im Shop fragen");
-			
 			objl.getObjectives().get(0).setActList(actl1);
 			
 			ActivityList actl2 = new ActivityList();
-			
 			actl2.createAct("Irgwass machen", "MACH WAS!!!!!!");
 			actl2.createAct("Test", "Dies ist ein Test");
-			
 			objl.getObjectives().get(1).setActList(actl2);
 			
 			
@@ -163,18 +163,87 @@ public class HowToUseDB {
 			
 			babbel.persist();
 
-			Patient vader = new Patient();
-			vader.setName("Vader");
-			vader.setPrename("Hans");
-			vader.setGender("m");
-			vader.setBirthdate(new Date());
-			vader.setStreet("Todesstern");
-			vader.setZip("99999");
-			vader.setCity("Universe");
+			//**********************************************************************
+			//Second patient
+			
+			//Set basic data for patient 2
+			Patient zimmermann = new Patient();
+			zimmermann.setName("Zimmermann");
+			zimmermann.setPrename("Monika");
+			zimmermann.setGender("w");
+			String dateString2 = "19.06.1989";
+			try {
+				zimmermann.setBirthdate(SDF.parse(dateString2));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			zimmermann.setStreet("Hauptstrasse 84");
+			zimmermann.setZip("3020");
+			zimmermann.setCity("Bern");
 
-			vader.persist();
+			zimmermann.persist();
+		
+			//Add image to profile
+			try {
+				byte[] data = Files.readAllBytes(path);
+				zimmermann.setPicture(data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
-			System.out.println("Added Tim & Vader as Patients!");
+			//Set diagnoses
+			DiagnosisList dl2 = new DiagnosisList();
+			zimmermann.setDiagnosisList(dl2);
+
+			//Set medication
+			DrugList drugl2 = new DrugList();
+			drugl2.initdrugs();
+			zimmermann.setDrugList(drugl2);
+			
+			//Notes generieren
+			NoteList notel2 = new NoteList();
+			notel2.createNote("Suizidal", "Patientin Zimmermann ist seit einigen Tagen wieder suizidal", true);
+			notel2.createNote("Arztwechsel", "Patientin Zimmermann hat ihren Psychiater gewechselt.", true);
+			notel2.createNote("Einkauf", "Patientin Zimmermann möchte keine Produkte aus dem Discounter", false);
+			zimmermann.setNoteList(notel2);
+			
+			//Ziele definierern
+			ObjectiveList objl2 = new ObjectiveList();
+			objl2.createObj("Telefonieren", "Kann mit Verwandten oder Bekannten längere Telefonate führen", 3);
+			objl2.createObj("Aufräumen", "Kann Wohnung alleine sauber halten.", 7);
+			objl2.createObj("Arzt", "Arzttermine selbstständig managen", 10);
+			zimmermann.setObjectiveList(objl2);
+			
+			//Aktivitäten für erstes Ziel
+			ActivityList actl3 = new ActivityList();
+			actl3.createAct("Dauer", "Telefonate sollen länger als 30 Minuten dauern");
+			actl3.createAct("Sinnvolle Gespräche", "Thema des Gesprächs soll nicht auf der Erkrankung beruhen");
+			objl2.getObjectives().get(0).setActList(actl3);
+			
+			//Aktivitäten für zweites Ziel
+			ActivityList actl4 = new ActivityList();
+			actl4.createAct("Staubsaugen", "Mindestens zweimal pro Woche saugen");
+			actl4.createAct("Geschirr", "Geschirrspülmaschine selbstständig ein- und ausräumen");
+			objl2.getObjectives().get(1).setActList(actl4);
+			
+			//Aktivitäten für drittes Ziel
+			ActivityList actl5 = new ActivityList();
+			actl5.createAct("Termin vereinbaren", "Termin mit Arzt für halbjährliches Blutbild vereinbaren");
+			actl5.createAct("Zusätzliches Leiden", "Arzt auf bereits längere andauernde Kopfschmerzen ansprechen");
+			objl2.getObjectives().get(2).setActList(actl5);
+			
+			//Verlauf der Aktivitäten 
+			ActivityRecordList actRecList2 = new ActivityRecordList();
+			Instant instant3 = Instant.parse("2017-12-03T10:15:30.00Z");
+			actRecList1.createActivityRecord(Date.from(instant3), 2, "Frage bezüglich dem zusätzlichen Leiden wurde nicht gestellt. Patientin zu verängstigt");
+			Instant instant4 = Instant.parse("2017-12-10T10:15:30.00Z");
+			actRecList1.createActivityRecord(Date.from(instant4), 6, "Telefonat und zusätzlichen Termin wegen Kopfschmerzen vereinbart (mit Hilfe!).");
+			actl2.getActivities().get(5).setActRecList(actRecList2);
+			
+			zimmermann.persist();
+
+
+			System.out.println("Added Tim Babbel & Monika Zimmermann as Patients!");
 		}
 
 	}
