73221e55b49996e00dd2f3b18746758d2ca0cb46
petim1 <32766548+petim1@users.noreply.github.com>
2018-01-12 12:44:16 +0100

    ActivityTest

--- test/ch/bfh/btx8081/w2017/blue/sophobia/model/ActivityTest.java
+++ test/ch/bfh/btx8081/w2017/blue/sophobia/model/ActivityTest.java
@@ -1,14 +1,45 @@
 package ch.bfh.btx8081.w2017.blue.sophobia.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.junit.Test;
+
+import ch.bfh.btx8081.w2017.blue.sophobia.persistence.DB;
 
 public class ActivityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addActivity() {
+		
+		Patient p1 = new Patient();
+		p1.setName("Tester");
+		p1.setPrename("Test");
+		p1.persist();
+		
+		ObjectiveList objl = new ObjectiveList();
+		objl.createObj("Test", "Testen der Funktionalität", 2);
+		
+		ActivityList actl = new ActivityList();
+		actl.createAct("Test", "Testen der Funktionalität");
+		
+		objl.getObjectives().get(0).setActList(actl);
+		
+		EntityManager em = DB.getEntityManager();
+
+		Query q1 = em.createQuery("select p from Activity p");
+		List<Activity> activities = q1.getResultList();
+		activities = activities.stream().filter(p -> p.getName().equals("Test")).collect(Collectors.toList());
+		
+		p1.delete();
+
+		assertEquals("select should return exactly one entry", 1, activities.size());
+		
 	}
 
 }
