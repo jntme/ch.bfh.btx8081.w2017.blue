4873d2e88d0c0bc63c3f4d353995dee237d7fd0c
ziegm <32767151+ziegm@users.noreply.github.com>
2018-01-14 16:55:30 +0100

    Write ActivityRecordTest.delete()

---
+++ test/ch/bfh/btx8081/w2017/blue/sophobia/model/ActivityRecordTest.java
@@ -0,0 +1,48 @@
+package ch.bfh.btx8081.w2017.blue.sophobia.model;
+
+import java.time.Instant;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import org.junit.Test;
+
+import ch.bfh.btx8081.w2017.blue.sophobia.persistence.DB;
+
+/**
+ * @author ziegm
+ */
+public class ActivityRecordTest {
+
+	@Test(expected = NoResultException.class)
+	public void deleteAnActivityRecord() {
+		// given
+		ActivityRecord testActRec = new ActivityRecord();
+		Instant instant = Instant.parse("2017-10-03T10:15:30.00Z");
+		testActRec.setDate(Date.from(instant));
+		testActRec.setDescription("Beschreibung Test");
+		testActRec.setSuccess(4);
+		persist(testActRec);
+		int testActRecId = testActRec.getArId();
+		
+		// when
+		testActRec.delete();
+		
+		// then
+		EntityManager em = DB.getEntityManager();
+		Query query = em.createQuery("select ar from ActivityRecord ar where ar.arId = :arId");
+		query.setParameter("arId", testActRecId);
+		query.getSingleResult();
+	}
+	
+	private void persist(ActivityRecord record) {
+		EntityManager em = DB.getEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		trans.begin();
+		em.persist(record);
+		trans.commit();
+	}
+}
