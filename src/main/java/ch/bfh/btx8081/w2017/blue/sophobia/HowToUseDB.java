c0e9fd8860dc746ad197a084af4ea1d1c6f61f95
Jonathan Meier <mail@jntme.ch>
2018-01-18 16:09:37 +0100

    fix nasty error in HowToUseDB.java

--- src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/HowToUseDB.java
+++ src/main/java/ch/bfh/btx8081/w2017/blue/sophobia/HowToUseDB.java
@@ -202,7 +202,7 @@ public class HowToUseDB {
             actRecList1.createActivityRecord(Date.from(instant3), 2, "Frage bezüglich dem zusätzlichen Leiden wurde nicht gestellt. Patientin zu verängstigt");
             Instant instant4 = Instant.parse("2017-12-10T10:15:30.00Z");
             actRecList1.createActivityRecord(Date.from(instant4), 6, "Telefonat und zusätzlichen Termin wegen Kopfschmerzen vereinbart (mit Hilfe!).");
-            actl2.getActivities().get(4).setActRecList(actRecList2);
+            actl2.getActivities().get(1).setActRecList(actRecList2);
 
             zimmermann.persist();
 
